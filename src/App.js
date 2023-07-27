57e06b8a864ba4f49b42464754a81a29773c797c
serenk1y <msergey.d3v@gmail.com>
2023-07-27 17:18:28 +0600

    Japanese food
    Japanese food

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import Header from './components/Layout/Header'
+import Meals from './components/Meals/Meals'
+import Cart from './components/Cart/Cart'
+import CartContextProvider from './store/CartContextProvider'
+
+function App() {
+  const [cartIsVisible, setCartIsVisible] = useState(false)
+
+  const showCartHandler = () => {
+    setCartIsVisible(true)
+  }
+  const hideCartHandler = () => {
+    setCartIsVisible(false)
+  }
+
+  return (
+    <CartContextProvider>
+      {cartIsVisible && <Cart onHideCart={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartContextProvider>
+  )
+}
+
+export default App
